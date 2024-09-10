ded15d0f7454570491f03784723c5a14e29f1653
Ackrilll <skawlsgus2@naver.com>
2024-09-10 11:43:21 +0900

    ㅑinit

---
+++ src/main/java/com/sparta/basicspringsession3/BasicSpringSession3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.basicspringsession3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringSession3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringSession3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/basicspringsession3/BasicSpringSession3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.basicspringsession3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringSession3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
